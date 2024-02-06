d49a6a55d2b8bdaf8e413cb2d49f11e638baa6ec
Pratyush Sarkar <pratyushsarkar41@gmail.com>
2024-02-07 01:59:43 +0530

    final app

--- src/components/tvdetails.jsx
+++ src/components/tvdetails.jsx
@@ -14,15 +14,14 @@ const Tvdetails = () => {
     <Card style={{ width: '18rem' }}>
       <Card.Img variant="top" src={`https://image.tmdb.org/t/p/original/${location1.state.tv?.poster_path}`} />
       <Card.Body>
-        <Card.Title>Title:- {location1.state.tv?.title}</Card.Title>
+        <Card.Title>Title:- {location1.state.tv?.name}</Card.Title>
         <Card.Text>
          Overview:- {location1.state.tv?.overview}
         </Card.Text>
       </Card.Body>
       <ListGroup className="list-group-flush">
-        <ListGroup.Item>Release date:- {location1.state.tv?.release_date}</ListGroup.Item>
+        <ListGroup.Item>Release date:- {location1.state.tv?.first_air_date}</ListGroup.Item>
         <ListGroup.Item>Original Language:- {location1.state.tv?.original_language}</ListGroup.Item>
-        <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
         <ListGroup.Item><Trailer1 location1={location1}/></ListGroup.Item>
       </ListGroup>
       
